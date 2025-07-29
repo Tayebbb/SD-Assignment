8b67df4e3dbc0558d1cd39d71fd63dd85dd434d3
Mohammed Tayeb <mohammedtayebibne@gmail.com>
2025-07-29 17:44:43 +0600

    random

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import PostPage from './pages/postpage.jsx';
 import AuthorProfilePage from './pages/AuthorProfilePage';
 import SectionPage from './pages/SectionPage.jsx';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SectionPage />} />
-      <Route path="/sub-section" element={<PostPage />} />
-      <Route path="/author/:authorId" element={<AuthorProfilePage />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<SectionPage />} />
+        <Route path="/sub-section" element={<PostPage />} />
+        <Route path="/author/:authorId" element={<AuthorProfilePage />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
